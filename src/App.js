1ce0900d6f7313724f60b0f491a863418f41195d
world-one <worldone85@gmail.com>
2022-03-12 22:32:43 +0900

    feat: add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import Main from "./components/pages/main/Main";
+import About from "./components/pages/about/About";
 import './css/common.css';
 
 const App = () => {
   return (
-    <>
-      <Main />
-    </>
+    <HashRouter>
+      <Routes>
+        <Route index element={<Main />} />
+        <Route path={'/about'} element={<About />} />
+      </Routes>
+    </HashRouter>
   )
 };
 
